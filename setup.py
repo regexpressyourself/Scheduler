01176d77bf848ab072376718a7b14adea5d801b1
Sam Messina <samuel.messina@gmail.com>
2015-01-28 17:35:46 -0600

    pyinstall trial

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(  name = "scheduler",
-        version = "0.1",
-        description = "Scheduler",
-        executables = [Executable("scheduler.py")] ,
-        )
